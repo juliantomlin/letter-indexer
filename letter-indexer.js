4119670a358aad61868c28191e0ab6fe97e782e2
Julian <julian@juliantomlindesign.com>
2019-04-03 20:06:01 +0000

    finished excercise

---
+++ letter-indexer.js
@@ -0,0 +1,20 @@
+function countLetters (string){
+ var output = {};
+ var letterIndex = []
+ var string = string.toLowerCase();
+ for (var i = 0; i < string.length; i++){
+   if (string[i] === " "){
+
+   }
+   else if (!output[string[i]]){
+     output[string[i]] = [i];
+   } else {
+    output[string[i]].push(i);
+   }
+
+ }return output;
+
+
+}
+
+console.log(countLetters("Lighthouse in the house"));
